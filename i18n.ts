92a2828017350dcd582f29537bf9e9b142c3906e
Lazar <lazar.trifunovic@boopro.tech>
2024-08-07 18:47:45 +0200

    feature(Localization):Added languages

--- i18n.ts
+++ i18n.ts
@@ -2,7 +2,7 @@ import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
 
 // Can be imported from a shared config
-const locales = ["sr"];
+const locales = ["sr", "en", "de", "fr"];
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
