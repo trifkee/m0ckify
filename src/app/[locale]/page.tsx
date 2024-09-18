a3ee7ac998c3d213a947dd4b6efda5da4acaf50c
Lazar <lazar.trifunovic@boopro.tech>
2024-09-19 00:56:40 +0200

    fixed errors

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -27,78 +27,80 @@ export default function Home({
   const slicedTitle = t("title").split(" ");
 
   return (
-    <main className="home">
-      <motion.nav
-        className="navigation"
-        initial={{
-          opacity: 0,
-          y: -10,
-        }}
-        animate={{
-          opacity: 1,
-          y: 0,
-        }}
-        transition={{
-          delay: 0.5,
-        }}
-      >
-        <Image className="logo" src={logo} alt="Mockify" />
-
-        <div className="ctas">
-          <Link className="login" href={"/login"}>
-            <IoPersonCircleOutline />
-            Log in
-          </Link>
-          <LanguagePicker variant="editor" locale={locale} />
-        </div>
-      </motion.nav>
-
-      <section className="home__header">
-        <p className="title">
-          {slicedTitle.map((n, i) => (
-            <motion.span
-              initial={{
-                y: -10,
-                opacity: 0,
-              }}
-              animate={{
-                y: 0,
-                opacity: 1,
-              }}
-              transition={{
-                duration: 0.5,
-                ease: "easeInOut",
-                delay: i * 0.1,
-              }}
-              key={i}
-            >
-              {n}
-            </motion.span>
-          ))}
-        </p>
-
-        <motion.div
-          className="ctas"
+    <>
+      <main className="home">
+        <motion.nav
+          className="navigation"
           initial={{
             opacity: 0,
+            y: -10,
           }}
           animate={{
             opacity: 1,
+            y: 0,
           }}
           transition={{
-            delay: 1,
+            delay: 0.5,
           }}
         >
-          {/* <Button className="learn-more" variant="editor">
+          <Image className="logo" src={logo} alt="Mockify" />
+
+          <div className="ctas">
+            <Link className="login" href={"/login"}>
+              <IoPersonCircleOutline />
+              Log in
+            </Link>
+            <LanguagePicker variant="editor" locale={locale} />
+          </div>
+        </motion.nav>
+
+        <section className="home__header">
+          <p className="title">
+            {slicedTitle.map((n, i) => (
+              <motion.span
+                initial={{
+                  y: -10,
+                  opacity: 0,
+                }}
+                animate={{
+                  y: 0,
+                  opacity: 1,
+                }}
+                transition={{
+                  duration: 0.5,
+                  ease: "easeInOut",
+                  delay: i * 0.1,
+                }}
+                key={i}
+              >
+                {n}
+              </motion.span>
+            ))}
+          </p>
+
+          <motion.div
+            className="ctas"
+            initial={{
+              opacity: 0,
+            }}
+            animate={{
+              opacity: 1,
+            }}
+            transition={{
+              delay: 1,
+            }}
+          >
+            {/* <Button className="learn-more" variant="editor">
             {t("learnMore")}
-          </Button> */}
-          <Link href={"/generate"}>
-            <Button className="get-started" variant="editor">
-              {t("getStarted")} <IoArrowForwardSharp />
-            </Button>
-          </Link>
-        </motion.div>
-      </section>
-    </main>
+            </Button> */}
+            <Link href={"/generate"}>
+              <Button className="get-started" variant="editor">
+                {t("getStarted")} <IoArrowForwardSharp />
+              </Button>
+            </Link>
+          </motion.div>
+        </section>
+      </main>
+    </>
   );
 }
