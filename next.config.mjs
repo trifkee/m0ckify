01bc264235eb11508cdf7753ab619abcf255022f
Lazar <lazar.trifunovic@boopro.tech>
2024-09-23 13:20:15 +0200

    feature(Testing): Added WDYR library for testing rerendering of the components

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 import createNextIntlPlugin from "next-intl/plugin";
 import path from "path";
 import { fileURLToPath } from "url";
+import injectWhyDidYouRender from "./lib/testing/wdyr.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,6 +12,10 @@ const withNextIntl = createNextIntlPlugin();
 const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "ui/styles")],
+  },
+  webpack: (config, context) => {
+    injectWhyDidYouRender(config);
+    return config;
   },
 };
 
