d4a3e062561c48ca341ac148f60d6d643bf4c76a
Lazar <lazar.trifunovic@boopro.tech>
2024-10-23 18:40:14 +0200

    feature(SEO): Added headings to the page for better SEO

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -37,161 +37,177 @@ export default function Home({
   const slicedTitle = t("title").split(" ");
 
   return (
-    <main className="home">
-      <motion.nav
-        className="navigation"
-        initial={{
-          opacity: 0,
-          y: -10,
-        }}
-        animate={{
-          opacity: 1,
-          y: 0,
-        }}
-        transition={{
-          delay: 0.5,
+    <>
+      <div
+        className="seo-block"
+        style={{
+          position: "absolute",
+          visibility: "hidden",
         }}
       >
-        <div className="logo">
-          <Image className="logo" src={logo} alt="Mockify" />
-        </div>
-
-        <div className="ctas">
-          <Link className="login" href={"/login"}>
-            <LucideUser />
-            Log in
-          </Link>
-          <LanguagePicker variant="editor" locale={locale} />
-        </div>
-      </motion.nav>
-
-      <div className="home__header">
-        <div className="info">
-          <h1 className="title">
-            {slicedTitle.map((n, i) => (
-              <Fragment key={i}>
-                <motion.span
-                  initial={{
-                    y: -10,
-                    opacity: 0,
-                  }}
-                  animate={{
-                    y: 0,
-                    opacity: 1,
-                  }}
-                  transition={{
-                    duration: 0.5,
-                    ease: "easeInOut",
-                    delay: i * 0.1,
-                  }}
-                  key={i}
-                >
-                  {n}
-                </motion.span>
-                {(i + 1) % 2 === 0 ? (
-                  <div style={{ height: "100%", width: "100%" }} />
-                ) : null}
-              </Fragment>
-            ))}
-          </h1>
-
-          <article className="chips">
-            <motion.div
-              initial={{
-                opacity: 0,
-                scale: 0.5,
-                rotateY: 15,
-                rotateX: 50,
-                rotateZ: 25,
-                left: "-20%",
-                bottom: "-100%",
-              }}
-              animate={animateProps}
-              transition={{ delay: 1 }}
-              className="chip blue"
-            >
-              Multi-Language Support
-            </motion.div>
-            <motion.div
-              initial={{
-                opacity: 0,
-                scale: 0.5,
-                rotateY: 35,
-                rotateX: 20,
-                rotateZ: 55,
-                left: "-50%",
-                bottom: "150%",
-              }}
-              animate={animateProps}
-              transition={{ delay: 1.25 }}
-              className="chip orange"
-            >
-              Custom made models
-            </motion.div>
-            <motion.div
-              initial={{
-                opacity: 0,
-                scale: 0.5,
-                rotateY: 5,
-                rotateX: 100,
-                rotateZ: 50,
-                left: "0%",
-                bottom: "10%",
-              }}
-              animate={animateProps}
-              transition={{ delay: 0.85 }}
-              className="chip red"
-            >
-              Real Time Edit
-            </motion.div>
-            <motion.div
-              initial={{
-                opacity: 0,
-                scale: 0.5,
-                rotateY: 12,
-                rotateX: 20,
-                rotateZ: 95,
-                left: "-50%",
-                bottom: "-120%",
-              }}
-              animate={animateProps}
-              transition={{ delay: 0.5 }}
-              className="chip purple"
-            >
-              Dynamic 3D Preview
-            </motion.div>
-            <motion.div
-              initial={{
-                opacity: 0,
-                scale: 0.5,
-                rotateY: 25,
-                rotateX: 10,
-                rotateZ: 25,
-                left: "-0%",
-                bottom: "-100%",
-              }}
-              animate={animateProps}
-              transition={{ delay: 1.25 }}
-              className="chip black"
-            >
-              Lighting & Environment Controls
-            </motion.div>
-          </article>
-        </div>
-
-        <div className="hero">
-          <Image src={heroImage} alt="" />
-        </div>
+        <h1>product mockup free online</h1>
+        <h2>how to do product mockup</h2>
+        <h2>product mockup free</h2>
+        <h2>product mockup generator</h2>
+        <h2>product mockup tool</h2>
+        <h2>how to do product mockup</h2>
       </div>
+      <main className="home">
+        <motion.nav
+          className="navigation"
+          initial={{
+            opacity: 0,
+            y: -10,
+          }}
+          animate={{
+            opacity: 1,
+            y: 0,
+          }}
+          transition={{
+            delay: 0.5,
+          }}
+        >
+          <div className="logo">
+            <Image className="logo" src={logo} alt="Mockify" />
+          </div>
 
-      <Link className="get-started" href={"/generate"}>
-        {t("getStarted")}
-        <LucideChevronRight />
-      </Link>
-      <div className="images">
-        <Image src={grids} alt="" />
-        <Image src={bgImage} alt="" />
-      </div>
-    </main>
+          <div className="ctas">
+            <Link className="login" href={"/login"}>
+              <LucideUser />
+              Log in
+            </Link>
+            <LanguagePicker variant="editor" locale={locale} />
+          </div>
+        </motion.nav>
+
+        <div className="home__header">
+          <div className="info">
+            <p className="title">
+              {slicedTitle.map((n, i) => (
+                <Fragment key={i}>
+                  <motion.span
+                    initial={{
+                      y: -10,
+                      opacity: 0,
+                    }}
+                    animate={{
+                      y: 0,
+                      opacity: 1,
+                    }}
+                    transition={{
+                      duration: 0.5,
+                      ease: "easeInOut",
+                      delay: i * 0.1,
+                    }}
+                    key={i}
+                  >
+                    {n}
+                  </motion.span>
+                  {(i + 1) % 2 === 0 ? (
+                    <div style={{ height: "100%", width: "100%" }} />
+                  ) : null}
+                </Fragment>
+              ))}
+            </p>
+
+            <article className="chips">
+              <motion.div
+                initial={{
+                  opacity: 0,
+                  scale: 0.5,
+                  rotateY: 15,
+                  rotateX: 50,
+                  rotateZ: 25,
+                  left: "-20%",
+                  bottom: "-100%",
+                }}
+                animate={animateProps}
+                transition={{ delay: 1 }}
+                className="chip blue"
+              >
+                Multi-Language Support
+              </motion.div>
+              <motion.div
+                initial={{
+                  opacity: 0,
+                  scale: 0.5,
+                  rotateY: 35,
+                  rotateX: 20,
+                  rotateZ: 55,
+                  left: "-50%",
+                  bottom: "150%",
+                }}
+                animate={animateProps}
+                transition={{ delay: 1.25 }}
+                className="chip orange"
+              >
+                Custom made models
+              </motion.div>
+              <motion.div
+                initial={{
+                  opacity: 0,
+                  scale: 0.5,
+                  rotateY: 5,
+                  rotateX: 100,
+                  rotateZ: 50,
+                  left: "0%",
+                  bottom: "10%",
+                }}
+                animate={animateProps}
+                transition={{ delay: 0.85 }}
+                className="chip red"
+              >
+                Real Time Edit
+              </motion.div>
+              <motion.div
+                initial={{
+                  opacity: 0,
+                  scale: 0.5,
+                  rotateY: 12,
+                  rotateX: 20,
+                  rotateZ: 95,
+                  left: "-50%",
+                  bottom: "-120%",
+                }}
+                animate={animateProps}
+                transition={{ delay: 0.5 }}
+                className="chip purple"
+              >
+                Dynamic 3D Preview
+              </motion.div>
+              <motion.div
+                initial={{
+                  opacity: 0,
+                  scale: 0.5,
+                  rotateY: 25,
+                  rotateX: 10,
+                  rotateZ: 25,
+                  left: "-0%",
+                  bottom: "-100%",
+                }}
+                animate={animateProps}
+                transition={{ delay: 1.25 }}
+                className="chip black"
+              >
+                Lighting & Environment Controls
+              </motion.div>
+            </article>
+          </div>
+
+          <div className="hero">
+            <Image src={heroImage} alt="" />
+          </div>
+        </div>
+
+        <Link className="get-started" href={"/generate"}>
+          {t("getStarted")}
+          <LucideChevronRight />
+        </Link>
+        <div className="images">
+          <Image src={grids} alt="" />
+          <Image src={bgImage} alt="" />
+        </div>
+      </main>
+    </>
   );
 }
