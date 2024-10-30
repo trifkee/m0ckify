92c5147301b52f99d8d9fa7a5a49c5d9a958e322
Lazar <lazar.trifunovic@boopro.tech>
2024-10-30 21:00:31 +0100

    fix(routing):Fixed links and routing between pages, fixed error with missing lang param in url

--- i18n.ts
+++ i18n.ts
@@ -1,8 +1,6 @@
 import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
-
-// Can be imported from a shared config
-const locales = ["sr", "en", "de", "fr", "ru", "ch"];
+import { locales } from "@/navigation";
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
