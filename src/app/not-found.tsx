4862ef1dfcacf0878508ad435d84fe78ed354326
Lazar <lazar.trifunovic@boopro.tech>
2024-11-06 10:59:36 +0100

    fix(not found): Fixed Error and not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,42 +1,52 @@
 "use client";
 
-import Button from "@/ui/components/atoms/Button.atom";
-
-import mockifyBackground from "@/public/images/bg.jpg";
-import logo from "@/public/images/logo.png";
-
-import "@/ui/styles/pages/notFound.page.scss";
 import Image from "next/image";
-import { useTranslations } from "next-intl";
 import Link from "next/link";
 
-export default function RootNotFound() {
-  <html lang="en">
-    <body>
-      <main className="not-found">
-        <div className="left">
-          <Link href={"/"}>
-            <Image src={logo} alt="Mockify" />
-          </Link>
+import Button from "@/ui/components/atoms/Button.atom";
 
-          <div className="text">
-            <h1>Something went wrong</h1>
-            <p>We couldn&apos;t find the page you are looking for...</p>
-          </div>
-          <div className="links">
+import { Inter } from "next/font/google";
+
+import logo from "@/public/images/logo.png";
+import mockifyBackground from "@/public/images/bg.jpg";
+
+import "@/ui/styles/pages/notFound.page.scss";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export default function NotFound() {
+  return (
+    <html lang="en">
+      <body className={` ${inter.className}  body`}>
+        <main className="not-found">
+          <div className="left">
             <Link href={"/"}>
-              <Button variant="primary">Home Page</Button>
+              <Image src={logo} alt="Mockify" />
             </Link>
-            <Link href={"/generate"}>
-              <Button variant="danger">Let&apos;s Work!</Button>
-            </Link>
+
+            <div className="text">
+              <h1>Something went wrong</h1>
+              <p>We couldn&apos;t find the page you are looking for...</p>
+            </div>
+            <div className="links">
+              <Link href={"/"}>
+                <Button variant="editor">Home Page</Button>
+              </Link>
+              <Link href={"/generate"}>
+                <Button variant="editor" className="danger">
+                  Let&apos;s Work!
+                </Button>
+              </Link>
+            </div>
           </div>
-        </div>
-        <div className="right">
-          <Image src={mockifyBackground} alt="Mockify" />
-        </div>
-      </main>
-      {/* <Error statusCode={404} /> */}
-    </body>
-  </html>;
+          <div className="right">
+            <Image src={mockifyBackground} alt="Mockify" />
+          </div>
+        </main>
+        {/* <Error statusCode={404} /> */}
+      </body>
+    </html>
+  );
 }
