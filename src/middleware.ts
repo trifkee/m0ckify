b1250f034159e4fb9679d561404d3aefd3a40d35
Lazar <lazar.trifunovic@boopro.tech>
2025-01-14 15:13:58 +0100

    fixes

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,5 +4,5 @@ import { routing } from "@/i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: ["/", "/(de|en|fr|sr|ch|ru)/:path*"],
+  matcher: ["/", "/(de|en|fr|sr|ch|ru|tr)/:path*"],
 };
